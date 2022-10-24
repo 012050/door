089f49a2459f0353155709d7dfcb0b508fbae08d
012050 <beomjunchoe@naver.com>
2022-10-25 00:34:24 +0900

    명령어 추가, 가독성 개선
    기존 정지 명령어 stop에서 exit추가, 다시 로그인하는 re, login 명령어 추가

--- main.py
+++ main.py
@@ -17,51 +17,57 @@ import time
 #경로 확인
 import os
 
+path1 = os.getcwd() + "\personal_info.txt"
+f = open(path1, "r")
 
-# f = open("C:\com\program\Python\door\personal_info.txt", "r")
-try:
-    path1 = os.getcwd() + "\personal_info.txt"
-    f = open(path1, "r")
+info_data = f.read()
+f.close()
 
-    info_data = f.read()
-    f.close()
+s_id = info_data.split("\n")[0]
+s_pw = info_data.split("\n")[1]
 
-    s_id = info_data.split("\n")[0]
-    s_pw = info_data.split("\n")[1]
+print(s_id,s_pw)
+options = Options()
 
-    print(s_id,s_pw)
-    options = Options()
+# driver = webdriver.Chrome("C:\\com\program\Python\door\chromedriver.exe")
 
-    # driver = webdriver.Chrome("C:\\com\program\Python\door\chromedriver.exe")
+path2 = os.getcwd() + "\chromedriver.exe"
+driver = webdriver.Chrome(path2)
 
-    path2 = os.getcwd() + "\chromedriver.exe"
-    driver = webdriver.Chrome(path2)
-    
-    driver.maximize_window()
-    
-    driver.get("https://door.deu.ac.kr/sso/login.aspx")
-    driver.implicitly_wait(60)
-    login = driver.find_element(By.XPATH,"/html/body/form/div[2]/div[1]/div/table/tbody/tr[1]/td[2]/input")
+driver.maximize_window()
 
-    login.send_keys(s_id)
+driver.get("https://door.deu.ac.kr/sso/login.aspx")
+driver.implicitly_wait(60)
 
-    pw = driver.find_element(By.CLASS_NAME,"i_text")
-    pw.send_keys(s_pw,Keys.RETURN)
-    driver.implicitly_wait(60)
-    driver.find_element(By.XPATH,'//*[@id="gnbContent"]/div/div[2]/ol[2]/li[3]/a').click()
-    driver.implicitly_wait(60)
+login = driver.find_element(By.XPATH,"/html/body/form/div[2]/div[1]/div/table/tbody/tr[1]/td[2]/input")
+login.send_keys(s_id)
 
-    # driver.find_element(By.CSS_SELECTOR,"td.tAlignL pad_10 font_wei_n").click()
-    print('\n\n\n\n\n\nif you want to stop, press ctrl + c or input "stop"\n:\n\n')
-    a = input()
-    
-    while 1:
-        a = input('\n\nif you want to stop, press ctrl + c or input "stop"\n:')
-        if a == "stop":
-            driver.close()
-            # break
-        if a == "re" or a == "main":
-            driver.get("http://door.deu.ac.kr/MyPage")
+pw = driver.find_element(By.CLASS_NAME,"i_text")
+pw.send_keys(s_pw,Keys.RETURN)
 
-except KeyboardInterrupt:
-    driver.close()
+driver.implicitly_wait(60)
+driver.find_element(By.XPATH,'//*[@id="gnbContent"]/div/div[2]/ol[2]/li[3]/a').click()
+driver.implicitly_wait(60)
+
+print('\n\n\n\n\n\nif you want to stop, press ctrl + c or input "stop"\n:\n\n')
+a = input()
+
+while 1:
+    a = input('\n\nif you want to stop, press ctrl + c or input "stop"\n:')
+    if a == "stop" or a == "exit":
+        driver.close()
+        break
+    elif a == "main":
+        driver.get("http://door.deu.ac.kr/MyPage")
+
+    elif a == "login" or a == "re":
+        driver.get("https://door.deu.ac.kr/sso/login.aspx")
+        driver.implicitly_wait(60)
+        login = driver.find_element(By.XPATH,"/html/body/form/div[2]/div[1]/div/table/tbody/tr[1]/td[2]/input")
+        login.send_keys(s_id)
+        pw = driver.find_element(By.CLASS_NAME,"i_text")
+        pw.send_keys(s_pw,Keys.RETURN)
+        driver.implicitly_wait(60)
+        driver.find_element(By.XPATH,'//*[@id="gnbContent"]/div/div[2]/ol[2]/li[3]/a').click()
+
+driver.close()
