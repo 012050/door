b293221c87fb3c762233748114f2b9e6da728ab3
012050 <beomjunchoe@naver.com>
2022-12-20 02:51:12 +0900

    정보 입력 기능 추가
    +암호화/복호화, 틀린 정보 감지/수정, 팝업창 닫기

--- main.py
+++ main.py
@@ -37,13 +37,21 @@ def dorm_login():
 
 
 # 텍스트 파일에서 아이디, 비밀번호 가져오기
-path1 = os.getcwd() + "\personal_info.txt"
-f = open(path1, "r")
-info_data = f.read()
-f.close()
+try:
+    path1 = os.getcwd() + "\personal_info.txt"
+    f = open(path1, "r")
+    info_data = f.read()
+    f.close()
+    s_id = info_data.split("\n")[0]
+    s_pw = info_data.split("\n")[1]
 
-s_id = info_data.split("\n")[0]
-s_pw = info_data.split("\n")[1]
+except:
+    print("Please enter your ID and password")
+    s_id = input("ID: ")
+    s_pw = input("PASSWORD: ")
+    p_file = open("personal_info.txt", "w")
+    p_file.write(s_id + "\n" + s_pw)
+    p_file.close()
 
 # 크롬드라이버 찾기
 path2 = os.getcwd() + "\chromedriver.exe"
