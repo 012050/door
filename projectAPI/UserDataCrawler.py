1fc24fe6f012e1d44d0d0ef3192d5931166757e0
012050 <beomjunchoe@naver.com>
2023-12-18 14:43:35 +0900

    .

---
+++ projectAPI/UserDataCrawler.py
@@ -0,0 +1,109 @@
+import os
+import dotenv
+import time
+import json
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+
+def GetUserData(ID, PW):
+    json_data = {}
+    driver = webdriver.Chrome()
+    driver.get("http://door.deu.ac.kr/sso/login.aspx")
+    driver.implicitly_wait(60)
+    login = driver.find_element(By.XPATH,"/html/body/form/div[2]/div[1]/div/table/tbody/tr[1]/td[2]/input")
+    login.send_keys(ID, Keys.TAB, PW, Keys.RETURN)
+    driver.implicitly_wait(60)
+    driver.find_element(By.XPATH,'//*[@id="gnbContent"]/div/div[2]/ol[2]/li[3]/a').click()
+    driver.implicitly_wait(60)
+    driver.find_element(By.ID, "btn_quick_close").click()
+    driver.implicitly_wait(10)
+
+    # 강의실 목록
+    lecture_list = driver.find_elements(By.XPATH, '//*[@id="wrap"]/div[2]/div[3]/div[3]/table/tbody/tr')
+    lecture_list.pop(0)
+    for lecture_number in range(len(lecture_list)):
+        lecture_click_list = f"/html/body/div[2]/div[2]/div[3]/div[3]/table/tbody/tr[{int(lecture_number) + 2}]/td[3]/a"
+        lecture_list_name = driver.find_element(By.XPATH, lecture_click_list)
+        lecture_name = lecture_list_name.text
+        json_data[lecture_name] = {}
+        lecture_list_name.click()
+        time.sleep(2)
+        # 과제 확인 코드
+        try:
+            print(f"과제 확인 코드 : {lecture_name}")
+            # 과제 버튼 클릭
+            driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[1]/div/div[3]/ul/li/ul/li[3]/a/span").click()
+            driver.implicitly_wait(10)
+            task_list = driver.find_elements(By.XPATH, '//*[@id="sub_content2"]/div/table/tbody/tr')
+            task_list.pop(0)
+            json_data[lecture_name]['task'] = []
+            if(task_list[0].text != "등록된 과제가 없습니다."):
+                for task_number in range(len(task_list)):
+                    driver.find_element(By.XPATH, f'//*[@id="sub_content2"]/div/table/tbody/tr[{task_number + 2}]/td[3]/a').click()
+                    print(f"{task_number + 1}번째 과제 확인")
+                    period = driver.find_element(By.XPATH, '//*[@id="sub_content2"]/div[1]/table/tbody/tr[2]/td[1]').text
+                    title = driver.find_element(By.XPATH, '//*[@id="sub_content2"]/div[1]/table/tbody/tr[3]/td').text
+                    content = driver.find_element(By.XPATH, '//*[@id="sub_content2"]/div[1]/table/tbody/tr[4]/td').text
+                    print(lecture_name)
+                    # json_data[lecture_name].append({ "task" : {"period" : period, "title" : title, "content" : content}})
+                    json_data[lecture_name]['task'].append({"period" : period, "title" : title, "content" : content})
+                    driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[1]/div/div[3]/ul/li/ul/li[3]/a/span").click()
+                    driver.implicitly_wait(10)
+            else:
+                print("else문")
+                print(task_list[0].text)
+                json_data[lecture_name]['task'] = None
+        except:
+            print(f"과제 확인 코드 오류 : {lecture_name}")
+        # 수업활동일지 확인 코드
+        try:
+            print("수업활동일지 확인 코드")
+            driver.find_element(By.XPATH, '//*[@id="lnbContent"]/div/div[4]/ul/li/ul/li[1]/a/span').click()
+            driver.implicitly_wait(10)
+            lecture_journal = driver.find_elements(By.XPATH, '//*[@id="sub_content2"]/div/table/tbody/tr')
+            lecture_journal.pop(0)
+            for i in lecture_journal:
+                print(i.text)
+            json_data[lecture_name]['journal'] = []
+            if(lecture_journal[0].text != "등록된 산출물이 없습니다."):
+                for journal_number in range(len(lecture_journal)):
+                    driver.find_element(By.XPATH,  f'//*[@id="sub_content2"]/div/table/tbody/tr[{journal_number + 2}]/td[2]/a').click()
+                    period = driver.find_element(By.XPATH, '//*[@id="sub_content2"]/div[1]/table/tbody/tr[2]/td[1]').text
+                    title = driver.find_element(By.XPATH, '//*[@id="sub_content2"]/div[1]/table/tbody/tr[3]/td').text
+                    content = driver.find_element(By.XPATH, '//*[@id="sub_content2"]/div[1]/table/tbody/tr[4]/td').text
+                    print(lecture_name)
+                    json_data[lecture_name]['journal'].append({"period" : period, "title" : title, "content" : content})
+                    driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[1]/div/div[3]/ul/li/ul/li[3]/a/span").click()
+                    driver.implicitly_wait(10)
+            else:
+                print("else문")
+                print(lecture_journal[0].text)
+                json_data[lecture_name]['journal'] = None
+
+        except:
+            print(f"수업활동일지 확인 코드 오류 : {lecture_name}")
+
+        # 강의실로 돌아가기
+        driver.get("http://door.deu.ac.kr/MyPage")
+        driver.implicitly_wait(10)
+
+    print("프로그램 종료")
+    driver.close()
+    file_name = f"{ID}.json"
+    with open(file_name, 'w') as json_file:
+        json.dump(json_data, json_file, indent=4)
+    return 0
+
+if __name__ == "__main__":
+    start_time = time.time()
+    dotenv.load_dotenv(dotenv.find_dotenv())
+    id = os.environ["ID"]
+    pw = os.environ["PW"]
+
+    GetUserData(ID=id, PW=pw)
+    
+    print("--- %s seconds ---" % (time.time() - start_time))
--- projectAPI/app.py
+++ projectAPI/app.py
@@ -1,35 +1,32 @@
-import os
-import dotenv
-import time
 import json
 
-from flask import Flask, jsonify, request
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-
-
-dotenv.load_dotenv(dotenv.find_dotenv())
-
-# ID = os.environ["ID"]
-# PW = os.environ["PW"]
-
+from flask import Flask, jsonify, request, render_template
+from UserDataCrawler import GetUserData
 app = Flask(__name__)
 
-json_data = {"test": "test"}
+@app.route("/", methods=["GET"])
+def MainPage():
+    return render_template("home.html")
 
-@app.route("/getdata", methods=["GET"])
-def UserDataSend():
-    return jsonify(json_data)
-
-@app.route("/getdata", methods=["POST"])
-def UserDataGet():
+@app.route("/getdata", methods=["POST", "GET"])
+def GetUserDataPage():
     ID = request.form.get("ID")
     PW = request.form.get("PASSWORD")
-    driver = webdriver.Chrome()
-    driver.close()
-    return 200
+
+    # POST 요청 - 데이터 저장 및 반환
+    if request.method == "POST":
+        GetUserData(ID, PW)
+        FileName = f'{ID}.json'
+        with open(FileName, 'r') as json_file:
+            json_data = json.load(json_file)
+        return jsonify(json_data)
+
+    # GET 요청 - 데이터 반환
+    elif request.method == "GET":
+        FileName = f'{ID}.json'
+        with open(FileName, 'r') as json_file:
+            json_data = json.load(json_file)
+        return jsonify(json_data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8080", debug=True)
